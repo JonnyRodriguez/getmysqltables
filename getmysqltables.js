18de14600262bcc5753d83b922ba67c359e087d7
“Jonny <jojoroto@gmail.com>
2019-05-23 02:04:59 +0200

    1

--- getmysqltables.js
+++ getmysqltables.js
@@ -1,43 +1,48 @@
-var mysql = require('mysql');
-var conn = mysql.createConnection({
-  user: 'root',
-  password: 'password',
-  database: 'turing'
-});
+#!/usr/bin/env node
 
-const query = sql => {
-  return new Promise( (resolve, reject) => {
-    conn.query(sql, function (err, data) {
-      if (err) return reject(err)
-      resolve(data)
-    })
-  })
+const mysql = require('mysql2/promise');
+const program = require('commander');
+
+program
+	.version('0.1')
+	.option('-d, --database <db>', 'mysql database', 'turing')
+	.option('-u, --user <user>', 'mysql user', 'root')
+	.option('-p, --password <password>', 'mysql password', 'password')
+	.option('-j, --json', 'json output')
+	.option('-H, --host <host>', 'mysql host', 'localhost')
+	.option('-P, --port <port>', 'mysql port', '3306')
+	.parse(process.argv);
+
+const db = {};
+const pool = mysql.createPool(pick(program, ['user', 'password', 'host', 'port']));
+
+pool.query(tablesQuery()).then(result => {
+	for (let row of result[0]) {
+		let { Table, ...column } = row;
+		if (column.Pos == 1) db[row.Table] = [];
+		db[row.Table].push(column);
+	}
+	console.log(program.json ? JSON.stringify(db) : db);
+}).then(() => pool.end());
+
+function tablesQuery() {
+	return `SELECT table_name as 'Table', \
+		column_name as 'Column', \
+		ordinal_position as 'Pos', \
+		column_type as 'Type', \
+		is_nullable, \
+		column_default as 'Default', \
+		extra \
+		FROM \
+		information_schema.columns \
+		WHERE table_schema='${program.database}' \
+		ORDER BY \
+		table_name, ordinal_position \
+	`;
 }
 
-const getTable = table => {
-  return query(`desc ${table}`).then( rows => {
-    var columns = [];
-    for( let row of rows ) {
-      var column = {};
-      for( let key in row )
-        column[key]=row[key];
-      columns.push(column);
-    }
-    return {table,columns};
-  });
+function pick(object, items) {
+	const result = {};
+	items.forEach(item => result[item] = object[item]);
+	return result;
 }
-
-query('show tables').then( rows => {
-  var tables = [];
-  for( let row of rows )
-    for( let table in row )
-      tables.push(row[table]);
-  return tables;
-}).then( tables => {
-  tablesQuery = tables.map( table => getTable(table) );
-  Promise.all(tablesQuery).then( data => {
-    console.log(JSON.stringify(data));
-    conn.end();
-  });
-});
-
